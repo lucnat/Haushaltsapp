97a7f432317f0521b977a6d0d92afddcc06799c1
Luca Naterop <luca.naterop@bluewin.ch>
2015-04-30 12:31:36 +0200

    Initiated meteor Project
    in folder haushaltsapp

---
+++ haushaltsapp/client/index.js
@@ -0,0 +1,24 @@
+Template.mainContainer.helpers({
+	'loggedIn': function(){
+		return !!Meteor.user();
+	}
+});
+
+Template.mainContainer.events({
+	'click button': function(event, template){
+		$('#myButton')
+	}
+});
+
+
+
+
+
+function integrator(f, a, b){
+
+}
+
+
+function(x){
+	return 1/2*x^2;
+}
